7eb4909b8f44fcc96d560e8805d316012df44225
Sergey Ermakov <rihlerstudy@gmail.com>
2026-03-29 16:49:16 +0300

    Исправление формирования симлекс таблицы и добавление метода решения

--- big_m_method/BigMMethod.py
+++ big_m_method/BigMMethod.py
@@ -1,24 +1,24 @@
 import numpy as np
 
+from simplex.Simplex import SimplexMethod
 from task_of_lp.Condtition import Condition
 from task_of_lp.Task import Task
 
 
 class BigMMethod:
     def __init__(self, system, free_variables, constraints,
-                 objective_function, M, task):
+                 objective_function, task):
         self.system = system
         self.free_variables = free_variables
         self.constraints = constraints
         self.objective_function = objective_function
-        self.M = M
         self.task = task
 
 
         self.table = None
         self.objective_function_m = None
-        self.cntRow = None
-        self.cntCol = None
+        self.cnt_row = None
+        self.cnt_col = None
 
         self.basis = None
         self.slacks = None
@@ -26,52 +26,53 @@ class BigMMethod:
         self.artificials = None
 
     def create_table(self):
-        self.cntRow = self.system.shape[0]
-        self.cntCol = self.system.shape[1]
+        self.cnt_row = self.system.shape[0]
+        self.cnt_col = self.system.shape[1]
         self.table = self.system[:]
 
 
         # Списки базисных и искусственных переменных
-        self.basis = np.zeros(self.cntRow, dtype=int)
+        self.basis = np.zeros(self.cnt_row, dtype=int)
         self.slacks = []
         self.surpluses = []
         self.artificials = []
 
 
-        for row in range(self.cntRow):
+        for row in range(self.cnt_row):
             # Добавление доп. переменной
             if self.constraints[row] == Condition.LessEq:
-                new_col = np.zeros(self.cntRow, dtype = float)
+                new_col = np.zeros(self.cnt_row, dtype = float)
                 new_col[row] = 1
                 np.column_stack((self.table, new_col))
-                self.basis[row] = (self.cntCol-1)
-                self.slacks.append(self.cntCol-1)
-                self.cntCol += 1
+                self.basis[row] = (self.cnt_col-1)
+                self.slacks.append(self.cnt_col-1)
+                self.cnt_col += 1
 
 
             # Добавление доп. и иск. переменных
             elif self.constraints[row] == Condition.GreaterEq:
-                new_col = np.zeros(self.cntRow, dtype=float)
+                new_col = np.zeros(self.cnt_row, dtype=float)
                 new_col[row] = -1
                 self.table = np.column_stack((self.table, new_col))
-                self.surpluses.append(self.cntCol - 1)
-                self.cntCol += 1
+                self.cnt_col = self.table.shape[1]
+                self.surpluses.append(self.cnt_col - 1)
 
 
-                new_col = np.zeros(self.cntRow, dtype=float)
+                new_col = np.zeros(self.cnt_row, dtype=float)
                 new_col[row] = 1
                 self.table = np.column_stack((self.table, new_col))
-                self.basis[row] = (self.cntCol-1)
-                self.artificials.append(self.cntCol - 1)
-                self.cntCol += 1
+                self.cnt_col = self.table.shape[1]
+                self.basis[row] = (self.cnt_col-1)
+                self.artificials.append(self.cnt_col - 1)
 
             else:
-                new_col = np.zeros(self.cntRow, dtype=float)
+                new_col = np.zeros(self.cnt_row, dtype=float)
                 new_col[row] = 1
                 self.table = np.column_stack((self.table, new_col))
-                self.basis[row] = (self.cntCol - 1)
-                self.artificials.append(self.cntCol - 1)
-                self.cntCol += 1
+                self.cnt_col += 1
+                self.basis[row] = (self.cnt_col - 1)
+                self.artificials.append(self.cnt_col - 1)
+
 
 
 
@@ -87,12 +88,12 @@ class BigMMethod:
         # Коэффициенты целевой функции, которые зависят M
         self.objective_function_m = np.zeros(self.table.shape[1], dtype=float)
         sum_free_var_m = 0
-        for row in range(self.cntRow):
+        for row in range(self.cnt_row):
             for artificial in self.artificials:
                 if self.table[row, artificial] == 1:
                     self.objective_function_m -= self.table[row]
                     self.objective_function_m[artificial] = 0
-                    sum_free_var_m += self.free_variables[row]
+                    sum_free_var_m -= self.free_variables[row]
 
         if self.task == Task.Max:
             self.objective_function_m *= -1
@@ -107,155 +108,37 @@ class BigMMethod:
         self.free_variables = np.hstack((self.free_variables, [0]))
         self.free_variables = np.hstack((self.free_variables, sum_free_var_m))
         self.table = np.column_stack((self.table, self.free_variables))
+        self.cnt_row = self.table.shape[0]
+        self.cnt_col = self.table.shape[1]
+
+    def solve(self):
+        self.create_table()
+        simplex = SimplexMethod(self.table, self.basis)
+
+        # Вывод с учетом всех строк
+        without = 0
+
+        has_solution, solution, z_m = simplex.solve(without,self.cnt_row - 1, False)
+        if has_solution:
+            for basis in self.basis:
+                for artificial in self.artificials:
+                    if basis == artificial:
+                        print("Система несовместна в области допустимых решений")
+                        return False, solution, z_m
+
+            without = 1
+            has_solution, solution, z_m = simplex.solve(without, self.cnt_row - 2, True)
+            return has_solution, solution, z_m
+
+        else:
+            return  has_solution, solution, z_m
 
 
 
-    def find_column(self, index):
-        last_row = self.table[index]
-        min_val = np.min(last_row)
-
-        if min_val >= 0:
-            return None
-
-        return np.argmin(last_row)
-
-    def find_row(self, column):
-        s = []
-        for row in range(self.cntRow):
-            if self.table[row, column] > 1e-10:
-                s_i = self.table[row, -1] / self.table[row, column]
-                s.append((s_i, row))
-
-        if not s:
-            return None
-
-        return min(s)[1]
-
-    def step(self, row, column):
-        pivot_element = self.table[row, column]
-        self.table[row] /= pivot_element
-
-        for i in range(self.cntRow):
-            if i != row:
-                current_pivot = self.table[i, column]
-                self.table[i] -= current_pivot * self.table[row]
-
-        self.basis[row] = column
-
-    def solve(self, verbose: bool = True):
-        if verbose:
-            print("=" * 60)
-            print("Продолжение по 1-ой оценочной строчке")
-            print("=" * 60)
-            self.print_table()
-
-        while True:
-            # Поиск ведущего столбца
-            column = self.find_column(self.cntRow - 2)
-            if column is None:
-                if verbose:
-                    print("\nОптимальное решение найдено!")
-                break
-
-            row = self.find_row(column)
-            if row is None:
-                if verbose:
-                    print("\nЗадача неограничена!")
-                return False, None, None
-
-            # Выполнение шага
-            if verbose:
-                print(f"\n--- Итерация {self.iterations + 1} ---")
-                print(f"Разрешающий столбец: {column + 1}")
-                print(f"Разрешающая строка: {row + 1}")
-            self.step(row, column)
-            if verbose:
-                self.print_table()
-
-        solution = np.zeros(self.n_vars + self.n_b)
-        for i in range(self.n_b):
-            solution[self.basis[i]] = self.table[i, -1]
-
-        z_value = self.table[-1, -1]
-        return True, solution, z_value
 
 
-    def solve_M(self, verbose: bool = True):
-        if verbose:
-            print("=" * 60)
-            print("По 2-ой оценочной строчке (коэф. при M)")
-            print("=" * 60)
-            self.print_table()
-
-        while True:
-            column = self.find_column(self.cntCol - 1)
-            if column is None:
-                for basis in range(self.cntRow):
-                    for artificial in self.artificials:
-                        if artificial == basis:
-                            print("\n Система не совместна")
-                        else:
-                            return self.solve()
-                break
-
-            row = self.find_row(column)
-            if row is None:
-                if verbose:
-                    print("\nЗадача не ограничена!")
-                return False, None, None
-
-            # Выполнение шага
-            if verbose:
-                print(f"\n--- Итерация {self.iterations + 1} ---")
-                print(f"Разрешающий столбец: {column + 1}")
-                print(f"Разрешающая строка: {row + 1}")
-            self.step(row, column)
-            if verbose:
-                self.print_table()
-
-        solution = np.zeros(self.cntCol)
-        for i in range(self.cntRow):
-            solution[self.basis[i]] = self.table[i, -1]
-
-        z_value = self.table[self.cntCol - 2, -1]
-
-        return True, solution, z_value
-
-    def print_table(self):
-        print("\nСимплекс-таблица:")
-        print("-" * 60)
 
 
-        header = "Базис | "
-        for i in range(self.cntCol):
-            header += f"     x{i + 1}"
-        header += "|  b  "
-        print(header)
-        print("-" * 60)
-
-
-        for i in range(self.cntRow):
-            row = f"  x{self.basis[i] :3} | "
-            for j in range(self.cntCol):
-                row += f"{self.table[i, j]:6.2f} "
-            row += f"| {self.table[i, -1]:6.2f}"
-            print(row)
-
-        print("-" * 60)
-
-        row = "  Z   | "
-        for j in range(self.cntCol):
-            row += f"{self.table[-2, j]:6.2f} "
-        row += f"| {self.table[-2, -1]:6.2f}"
-        print(row)
-        print("-" * 60)
-
-        row = "  Zm   | "
-        for j in range(self.cntCol):
-            row += f"{self.table[-1, j]:6.2f} "
-        row += f"| {self.table[-1, -1]:6.2f}"
-        print(row)
-        print("-" * 60)
 
 
 
